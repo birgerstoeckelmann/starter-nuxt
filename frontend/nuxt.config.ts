00690b1df3cfe3bd2b2b47bfecacf34640a57353
Birger Stöckelmann <bist@dcc.ruhr>
2025-12-16 08:31:49 +0100

    Seting up first pages

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,46 +3,49 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  app: {
-    head: {
-      titleTemplate: '%s | ' + process.env.SITE_NAME,
-      title: process.env.SITE_NAME
-    }
-  },
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000
-  },
-  runtimeConfig: {
-    public: {
-      GRAPHQL_TOKEN: process.env.GRAPHQL_TOKEN,
-      CRAFT_URL: process.env.CRAFT_URL,
-      BASE_URL: process.env.BASE_URL,
-      SITE_NAME: process.env.SITE_NAME
-    }
-  },
-  vite: {
-    plugins: [
-      tailwindcss()
+    app: {
+        head: {
+            titleTemplate: '%s | ' + process.env.SITE_NAME,
+            title: process.env.SITE_NAME
+        }
+    },
+    css: [
+        '@/assets/styles/globals.css'
     ],
-    server: {
-      hmr: {
-        protocol: 'wss',
-        host: process.env.BASE_URL?.replace('https://', ''),
-        port: 3000
-      },
-      allowedHosts: [
-        'starter-nuxt.ddev.site',
-        '.ddev.site'
-      ]
-    }
-  },
-  nitro: {
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
     devServer: {
-      watch: ['./server']
-    }
-  },
-  modules: ['@nuxtjs/i18n'],
+        host: '0.0.0.0',
+        port: 3000
+    },
+    runtimeConfig: {
+        public: {
+            GRAPHQL_TOKEN: process.env.GRAPHQL_TOKEN,
+            CRAFT_URL: process.env.CRAFT_URL,
+            BASE_URL: process.env.BASE_URL,
+            SITE_NAME: process.env.SITE_NAME
+        }
+    },
+    vite: {
+        plugins: [
+            tailwindcss()
+        ],
+        server: {
+            hmr: {
+                protocol: 'wss',
+                host: process.env.BASE_URL?.replace('https://', ''),
+                port: 3000
+            },
+            allowedHosts: [
+                'starter-nuxt.ddev.site',
+                '.ddev.site'
+            ]
+        }
+    },
+    nitro: {
+        devServer: {
+            watch: ['./server']
+        }
+    },
+    modules: ['@nuxtjs/i18n']
 })
