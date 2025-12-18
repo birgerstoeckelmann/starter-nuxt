5f847d0a567d02d274b515480a04a929028289f1
Birger Stöckelmann <bist@dcc.ruhr>
2025-12-18 08:52:08 +0100

    Types

--- frontend/app/types/data.d.ts
+++ frontend/app/types/data.d.ts
@@ -1,5 +1,16 @@
 interface GlobalData {
+  id: number;
+  title: string;
   logo: Image;
+  address: {
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    countryCode: string;
+    locality: string;
+    postalCode: number;
+    title: string;
+  };
 }
 
 interface Category {
