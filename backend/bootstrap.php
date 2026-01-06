0f70c1073b847a31ccf50548d6496e044a6d4ef4
Birger Stöckelmann <bist@dcc.ruhr>
2026-01-06 09:29:41 +0100

    setting error_reporting from https://github.com/craftcms/cms/issues/16606 in bootstrap.php

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -3,6 +3,9 @@
  * Shared bootstrap file
  */
 
+$errorLevel = E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED;
+error_reporting($errorLevel);
+
 // Define path constants
 define('CRAFT_BASE_PATH', __DIR__);
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
